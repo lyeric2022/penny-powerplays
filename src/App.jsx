b46a2d088cc40d69d577b39058e0116dc6857605
lyeric2022 <111820150+lyeric2022@users.noreply.github.com>
2023-06-16 04:21:27 -0700

    unoptimized, way unfinished code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,156 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set, onValue, onDisconnect, remove } from 'firebase/database';
+import { getAuth, signInAnonymously, onAuthStateChanged } from 'firebase/auth';
+
+import { createName } from './components/functions';
+
+// Firebase configuration
+const firebaseConfig = {
+  // Your Firebase configuration
+  apiKey: "AIzaSyBgjtQU9Kdw1v1ZgY0iHL9WfV8ao8RCMfg",
+  authDomain: "penny-powerplays.firebaseapp.com",
+  projectId: "penny-powerplays",
+  storageBucket: "penny-powerplays.appspot.com",
+  messagingSenderId: "91939738441",
+  appId: "1:91939738441:web:8a1216a1430a1457690098",
+  measurementId: "G-ZWS3GQ0JCE"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const database = getDatabase(firebaseApp);
+const auth = getAuth(firebaseApp);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [leaderboard, setLeaderboard] = useState([]);
+  const [nameInput, setNameInput] = useState('');
+  const [contributionInput, setContributionInput] = useState('');
+
+
+  useEffect(() => {
+    // Check if the user is signed in anonymously
+    onAuthStateChanged(auth, (user) => {
+      setIsSignedIn(!!user);
+
+      if (user) {
+        // Add user reference to the Realtime Database
+        const userRef = ref(database, `users/${user.uid}`);
+        set(userRef, {
+          anonymous: true,
+          name: createName(),
+          money: 10000,
+          hasContributed: "Unready",
+          isAlive: "Alive",
+        })
+          .then(() => {
+            console.log('User reference added to the database');
+          })
+          .catch((error) => {
+            console.log('Error adding user reference:', error);
+          });
+      }
+    });
+
+    // Fetch the leaderboard data from the database
+    const leaderboardRef = ref(database, 'users');
+    onValue(leaderboardRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const leaderboardData = Object.entries(data).map(([userId, userData]) => ({
+          id: userId,
+          name: userData.name,
+          money: userData.money,
+          hasContributed: userData.hasContributed,
+          isAlive: userData.isAlive,
+        }));
+        setLeaderboard(leaderboardData);
+      }
+    });
+
+    // Remove user reference from the Realtime Database on disconnect
+    const user = auth.currentUser;
+    if (user) {
+      const userRef = ref(database, `users/${user.uid}`);
+      onDisconnect(userRef).remove()
+        .then(() => {
+          console.log('User reference removed on disconnect');
+        })
+        .catch((error) => {
+          console.log('Error removing user reference on disconnect:', error);
+        });
+    }
+
+  }, [isSignedIn, auth, database]);
+
+  const handleNameChange = () => {
+    if (nameInput.trim() !== '') {
+      const user = auth.currentUser;
+      if (user) {
+        const userRef = ref(database, `users/${user.uid}`);
+        set(userRef, { ...leaderboard.find((player) => player.id === user.uid), name: nameInput })
+          .then(() => {
+            console.log('User name updated in the database');
+          })
+          .catch((error) => {
+            console.log('Error updating user name:', error);
+          });
+      }
+    }
+  };
+
+  const handleContributionChange = () => {
+    if (contributionInput.trim() !== '') {
+      const user = auth.currentUser;
+      if (user) {
+        const userRef = ref(database, `users/${user.uid}`);
+        set(userRef, {
+          ...leaderboard.find((player) => player.id === user.uid),
+          money: leaderboard.find((player) => player.id === user.uid).money - parseInt(contributionInput, 10),
+        })
+          .then(() => {
+            console.log('User money updated in the database');
+          })
+          .catch((error) => {
+            console.log('Error updating user money:', error);
+          });
+      }
+    }
+  };
 
   return (
-    <>
+    <div>
+      <h2>Leaderboard</h2>
+      <ul>
+        {leaderboard.map((player) => (
+          <p key={player.id}>
+            {player.name} | {player.isAlive} | ${player.money} | {player.hasContributed}
+          </p>
+        ))}
+      </ul>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input
+          type="text"
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+          placeholder="Enter your name"
+        />
+        <button onClick={handleNameChange}>Change Name</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <input
+          type="number"
+          min="0"
+
+          value={contributionInput}
+          onChange={(e) => setContributionInput(e.target.value)}
+          placeholder="Enter your contributions"
+        />
+        <button onClick={handleContributionChange}>Submit Contributions</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/functions.js
@@ -0,0 +1,44 @@
+function randomFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  
+  export function createName() {
+    const prefix = randomFromArray([
+      "COOL",
+      "SUPER",
+      "HIP",
+      "SMUG",
+      "COOL",
+      "SILKY",
+      "GOOD",
+      "SAFE",
+      "DEAR",
+      "DAMP",
+      "WARM",
+      "RICH",
+      "LONG",
+      "DARK",
+      "SOFT",
+      "BUFF",
+      "DOPE",
+    ]);
+    const animal = randomFromArray([
+      "BEAR",
+      "DOG",
+      "CAT",
+      "FOX",
+      "LAMB",
+      "LION",
+      "BOAR",
+      "GOAT",
+      "VOLE",
+      "SEAL",
+      "PUMA",
+      "MULE",
+      "BULL",
+      "BIRD",
+      "BUG",
+    ]);
+    return `${prefix} ${animal}`;
+  }
+  
